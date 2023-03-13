9d7903e3917c9db0221dd400e4a04a5387a273a1
Pawan Kumar Mehta <87040096+pawanpk87@users.noreply.github.com>
2023-03-13 21:09:13 +0530

    Added solution - LeetHub

---
+++ Tower Of Hanoi - GFG/tower-of-hanoi.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // You need to complete this function
+    int count = 0;
+    // avoid space at the starting of the string in "move disk....."
+    long long toh(int N, int from, int to, int aux) {
+        if(N == 0)
+            return count;
+        
+        toh(N-1,from,aux,to);
+        count++;
+        cout << "move disk " << N << " from rod " << from << " to rod " << to << endl;
+        toh(N-1,aux,to,from);
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;//testcases
+    while (T--) {
+        
+        int N;
+        cin >> N;//taking input N
+        
+        //calling toh() function
+        Solution ob;
+        
+        cout << ob.toh(N, 1, 3, 2) << endl;
+    }
+    return 0;
+}
+
+
+//Position this line where user code will be pasted.
+// } Driver Code Ends
