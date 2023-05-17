bbc0e6fe29eca4b0800e2e3b683d4da32a43b924
Pawan Kumar Mehta <87040096+pawanpk87@users.noreply.github.com>
2023-05-17 10:34:03 +0530

    Time: 477 ms (74.74%), Space: 0B (100.00%) - LeetHub

---
+++ 0595-big-countries/0595-big-countries.sql
@@ -0,0 +1,17 @@
+SELECT
+       name, population, area
+FROM 
+       World
+WHERE
+       area >= 3000000
+       
+UNION
+
+SELECT
+       name, population, area
+FROM 
+       World
+WHERE
+       population  >= 25000000
+;    
+       
