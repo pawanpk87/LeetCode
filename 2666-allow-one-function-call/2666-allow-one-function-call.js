9cc83294c78cd315282a21dcc0a96e17da12b3ee
Pawan Kumar Mehta <87040096+pawanpk87@users.noreply.github.com>
2023-05-19 11:30:17 +0530

    Time: 47 ms (97.26%), Space: 41.8 MB (70.35%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let count = 0;
+    return function(...args){
+        if(count == 0){
+            count++
+            return fn(...args);
+        }
+        else
+            return undefined;
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
