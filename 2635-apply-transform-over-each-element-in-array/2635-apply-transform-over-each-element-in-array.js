c58f747d44b98dbfb60129dfce7d69fbc7864ff3
Pawan Kumar Mehta <87040096+pawanpk87@users.noreply.github.com>
2023-05-19 11:02:07 +0530

    Time: 66 ms (15.98%), Space: 42 MB (47.53%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    return arr.map((num,i)=>{
+        return fn(num,i);
+    });
+};
