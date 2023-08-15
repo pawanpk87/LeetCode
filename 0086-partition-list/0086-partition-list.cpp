93d8daa7e1cc728805f716e41e53419e8bfe1827
Pawan Kumar Mehta <87040096+pawanpk87@users.noreply.github.com>
2023-08-15 22:01:56 +0530

    Time: 6 ms (50.56%), Space: 10.4 MB (9.03%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* newFinalList = new ListNode(-1);
+        ListNode* newList = new ListNode(-1);
+        
+        newFinalList->next = head;
+        
+        ListNode* currNode = head;
+        ListNode* prevNode = newFinalList;
+        ListNode* currNewList = newList;
+        
+        while(currNode){
+            if(currNode->val < x){
+                prevNode = currNode;                
+            }else{
+                currNewList->next = new ListNode(currNode->val);
+                currNewList = currNewList->next;
+                prevNode->next = currNode->next;
+            }
+            currNode = currNode->next;
+        }
+        
+        if(prevNode){
+            prevNode->next = newList->next;
+        }
+        
+        return newFinalList->next;
+    }
+};
+
+/*
+(newFinalList)->1 -> 4 -> 2 -> 5 -> 2
+                          | 
+   
+   
+   newFinalList =
+   newList = 0->4->3->5
+   
+   currNode = 3
+   
+   prevNode = 2
+   
+   
+   
+   
+
+*/
