0f181ab475beaf73bc4024cf6494cf5ffecf83da
Pawan Kumar Mehta <87040096+pawanpk87@users.noreply.github.com>
2023-10-20 10:24:56 +0530

    Added solution - LeetHub

---
+++ DFS of Graph - GFG/dfs-of-graph.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj =
+                new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public void dfs(int currNode, ArrayList<ArrayList<Integer>> adj, boolean visited[],ArrayList<Integer> result){
+        visited[currNode] = true;
+        
+        result.add(currNode);
+        
+        for(int adjNode : adj.get(currNode)){
+            if(!visited[adjNode]){
+                dfs(adjNode, adj, visited, result);
+            }
+        }
+    }
+    
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        boolean visited[] = new boolean[V];
+        
+        for(int i = 0; i < V; i++){
+            if(visited[i] == false){
+                dfs(i, adj, visited, result);
+            }
+        }
+        
+        return result;
+    }
+}
