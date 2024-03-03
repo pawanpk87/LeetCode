15900248c879d0c125f6a66074bb2a1cf1b54926
Pawan Kumar Mehta <87040096+pawanpk87@users.noreply.github.com>
2024-03-03 11:03:20 +0530

    Time: 68 ms (40.00%), Space: 58.2 MB (20.00%) - LeetHub

--- 3071-minimum-operations-to-write-the-letter-y-on-a-grid/3071-minimum-operations-to-write-the-letter-y-on-a-grid.cpp
+++ 3071-minimum-operations-to-write-the-letter-y-on-a-grid/3071-minimum-operations-to-write-the-letter-y-on-a-grid.cpp
@@ -27,13 +27,6 @@ private:
         }
         
         
-        // for(int i = 0; i < n; i++){
-        //     for(int j = 0; j < n; j++){
-        //         cout<<grid[i][j]<< " ";
-        //     }
-        //     cout<<endl;
-        // }
-        
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] != -1 && grid[i][j] != notY){
