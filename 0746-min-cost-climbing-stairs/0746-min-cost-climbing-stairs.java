e19132a5c1ad612eb95e7b2de2dbf7ddf9bb3524
Pawan Kumar Mehta <87040096+pawanpk87@users.noreply.github.com>
2024-05-19 16:40:06 +0530

    Time: 1 ms (39.52%), Space: 43.4 MB (18.12%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,30 +1,16 @@
 class Solution {
-    int[] memo;
-    
-    private int solve(int[] cost, int index, int n){
-        if(index >= n){
-            return 0;
-        }
-        
-        if(memo[index] != -1){
-            return memo[index];
-        }
-        
-        int pos1 = solve(cost, index+1, n);
-        int pos2 = solve(cost, index+2, n);
-        
-        return memo[index] = (cost[index] + Math.min(pos1, pos2));
-    }
-    
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
         
-        memo = new int[n];
-        Arrays.fill(memo, -1);
+        int[] dp = new int[n];
         
-        int pos1 = solve(cost, 0, n);
-        int pos2 = solve(cost, 1, n);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
         
-        return Math.min(pos1, pos2);
+        for(int i = 2; i < n; i++){
+            dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
+        }
+        
+        return Math.min(dp[n-1], dp[n-2]);
     }
 }
