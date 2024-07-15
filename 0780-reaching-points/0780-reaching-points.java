e2749b79a072632a6eadc6196572a3ab2d234eb9
Pawan Kumar Mehta <87040096+pawanpk87@users.noreply.github.com>
2024-07-15 21:18:15 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (46.39%) - LeetHub

---
+++ 0780-reaching-points/0780-reaching-points.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean reachingPoints(int sx, int sy, int tx, int ty) {
+        while(tx >= sx && ty >= sy){
+            if(tx > ty) {
+                if(sy == ty) {
+                    return (tx - sx) % ty == 0;
+                }
+                tx = tx % ty;
+            } else {
+                if(sx == tx) {
+                    return (ty - sy) % tx == 0;
+                }
+                ty = ty % tx;
+            }
+        }
+        return false;
+    }
+}
