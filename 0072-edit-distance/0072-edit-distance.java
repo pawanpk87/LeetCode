76c5685f05ee9ec7f7dba34a84842a07b0368e7f
Pawan Kumar Mehta <87040096+pawanpk87@users.noreply.github.com>
2024-07-18 14:21:40 +0530

    Time: 5 ms (65.43%), Space: 44.8 MB (52.80%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,47 +1,29 @@
 class Solution {
-    int[][] memo;
-    
-    private int solve(int i, int j, String word1, String word2, int  n, int m) {
-        if(i == n || j == m) {
-            if(i == n && j == m) {
-                return 0;
-            }
-            if(i == n && j != m) {
-                return (m - j);
-            } else {
-                return (n - i);
-            }
-        }
-        
-        if(memo[i][j] != -1){
-            return memo[i][j]; 
-        }
-        
-        if(word1.charAt(i) == word2.charAt(j)) {
-            return memo[i][j] = solve(i + 1, j + 1, word1, word2, n, m);
-        } else {
-            // Insert a char
-            int pos1 = 1 + solve(i, j + 1, word1, word2, n, m);
-            
-            // Delete a char
-            int pos2 = 1 + solve(i + 1, j, word1, word2, n, m);
-            
-            // Repla a char
-            int pos3 = 1 + solve(i + 1, j + 1, word1, word2, n, m);
-            
-            return memo[i][j] = Math.min(pos1, Math.min(pos2, pos3));
-        }
-    }
-    
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
         
-        memo = new int[n+1][m+1];
-        for(int i = 0; i < n; i++) {
-            Arrays.fill(memo[i], -1);
+        int[][] dp = new int[n+1][m+1];
+        
+        for(int j = 1; j <= m; j++) {
+            dp[0][j] = j;
+        }
+        for(int i = 0; i <= n; i++) {
+            dp[i][0] = i;
         }
         
-        return solve(0, 0, word1, word2, n, m);
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                char ch1 = word1.charAt(i - 1);
+                char ch2 = word2.charAt(j - 1);
+                if(ch1 == ch2) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
+                }
+            }
+        }
+        
+        return dp[n][m];
     }
 }
