3893511f250ff64396d7062749dc1cbaca80c81c
Pawan Kumar Mehta <87040096+pawanpk87@users.noreply.github.com>
2024-07-22 14:31:59 +0530

    Time: 9 ms (53.75%), Space: 45.1 MB (82.91%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,4 +1,4 @@
-class People implements Comparable<People>{
+class People{
     public String name;
     public Integer height;
     
@@ -6,29 +6,35 @@ class People implements Comparable<People>{
         this.name = name;
         this.height = height;
     }
-    
-    @Override
-    public int compareTo(People p) {
-        if(this.height > p.height) {
-            return -1;
-        } else if(this.height < p.height) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
 }
 
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
         List<People> peoples = new ArrayList<>();  
+        
         for(int i = 0; i < heights.length; i++) {
             peoples.add(new People(names[i], heights[i]));
         }
-        Collections.sort(peoples);
+        
+        Comparator<People> nameComparator = new Comparator<People>() {
+            @Override
+            public int compare(People p1, People p2) {
+                if(p1.height > p2.height) {
+                    return -1;
+                } else if(p1.height < p2.height) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        };
+        
+        Collections.sort(peoples, nameComparator);
+        
         for(int i = 0; i < names.length; i++) {
             names[i] = peoples.get(i).name;
         }
+        
         return names;
     }
 }
