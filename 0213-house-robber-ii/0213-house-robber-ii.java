a53605f7c0310e78c97cc3d28af94a95871e00ac
Pawan Kumar Mehta <87040096+pawanpk87@users.noreply.github.com>
2024-08-01 09:28:09 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (14.74%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -2,18 +2,16 @@ class Solution {
     private int[] memo;
     
     private int solve(int currHouse, int[] nums, int n){
-        if(currHouse >= n) {
-            return 0;
-        }    
+        int[] dp = new int[n+1];
+
+        dp[0] = currHouse == 1 ? 0 : nums[0];
+        dp[1] = nums[1];
         
-        if(memo[currHouse] != -1) {
-            return memo[currHouse];
+        for(int i = currHouse + 1; i < n; i++) {
+            dp[i] = Math.max(nums[i] + (i > 1 ? dp[i-2] : 0), dp[i-1]);
         }
         
-        int pos1 = nums[currHouse] + solve(currHouse + 2, nums, n);
-        int pos2 = solve(currHouse + 1, nums, n);
-        
-        return memo[currHouse] = Math.max(pos1, pos2);
+        return dp[n-1];
     }
     
     public int rob(int[] nums) {
@@ -23,13 +21,8 @@ class Solution {
             return nums[0];
         }
         
-        memo = new int[n];
-        
-        Arrays.fill(memo, -1);
-        int pos1 = solve(0, nums, n - 1);
-        
-        Arrays.fill(memo, -1);
         int pos2 = solve(1, nums, n);
+        int pos1 = solve(0, nums, n-1);
         
         return Math.max(pos1, pos2);
     }
