d961bdad620843230147141b1f1cfdc19dd187bc
P1 <87040096+pawanpk87@users.noreply.github.com>
2024-08-21 21:59:58 +0530

    Time: 1 ms (22.93%), Space: 41.5 MB (78.61%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -3,43 +3,39 @@ class Solution {
         int n = obstacleGrid.length;
         int m = obstacleGrid[0].length;
         
-        if(obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1){
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1) {
             return 0;
         }
         
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(obstacleGrid[i][j] == 1){
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(obstacleGrid[i][j] == 1) {
                     obstacleGrid[i][j] = -1;
                 }
             }
         }
         
-        for(int j = 0; j < m; j++){
-            if(obstacleGrid[0][j] == -1){
-                break;
-            }
+        for(int j = 0; j < m; j++) {
+            if(obstacleGrid[0][j] == -1) break;
             obstacleGrid[0][j] = 1;
         }
         
-        for(int i = 0; i < n; i++){
-            if(obstacleGrid[i][0] == -1){
-                break;
-            }
+        for(int i = 0; i < n; i++) {
+            if(obstacleGrid[i][0] == -1) break;
             obstacleGrid[i][0] = 1;
         }
         
-        for(int i = 1; i < n; i++){
-            for(int j = 1; j < m; j++){
-                if(obstacleGrid[i][j] == 0){
-                    if(obstacleGrid[i-1][j] == -1 || obstacleGrid[i][j-1] == -1){
-                        obstacleGrid[i][j] = Math.max(obstacleGrid[i-1][j], obstacleGrid[i][j-1]);
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j < m; j++) {
+                if(obstacleGrid[i][j] == 0) {
+                    if(obstacleGrid[i][j-1] == -1 || obstacleGrid[i-1][j] == -1) {
+                        obstacleGrid[i][j] = Math.max(obstacleGrid[i][j-1], obstacleGrid[i-1][j]);
                     } else {
-                        obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
+                        obstacleGrid[i][j] = obstacleGrid[i][j-1] + obstacleGrid[i-1][j];
                     }
                 }
             }
-        }        
+        }
         
         return Math.max(obstacleGrid[n-1][m-1], 0);
     }
