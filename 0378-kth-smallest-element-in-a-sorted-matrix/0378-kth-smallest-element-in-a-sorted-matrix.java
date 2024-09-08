1f73baefd33620676938a06cde8d4112874b734c
P1 <87040096+pawanpk87@users.noreply.github.com>
2024-09-08 12:17:54 +0530

    Time: 14 ms (56.50%), Space: 47.9 MB (83.20%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,24 +1,42 @@
+class Num implements Comparable<Num> {
+    int row;
+    int col;
+    int val;
+    
+    public Num(int row, int col, int val) {
+        this.row = row;
+        this.col = col;
+        this.val = val;
+    }
+    
+    @Override
+    public int compareTo(Num other) {
+        return this.val - other.val;
+    }
+}
+
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         int n = matrix.length;
-        int m = matrix[0].length;
         
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(new Comparator<Integer>(){
-            @Override
-            public int compare(Integer a, Integer b) {
-                return b - a;
-            }
-        });
+        PriorityQueue<Num> minHeap = new PriorityQueue<>();
         
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                maxHeap.add(matrix[i][j]);
-                if(maxHeap.size() > k) {
-                    maxHeap.poll();
-                }
-            }
+        for(int j = 0; j < n; j++) {
+            minHeap.add(new Num(0, j, matrix[0][j]));
         }
         
-        return maxHeap.poll();
+        // min1 {min2 , m3}
+        for(int i = 0; i < k-1; i++) {
+            Num curr = minHeap.poll();
+            
+            int row = curr.row;
+            int col = curr.col;
+            
+            if(row == n-1) continue;
+            
+            minHeap.add(new Num(row + 1, col, matrix[row+1][col]));
+        }
+        
+        return minHeap.poll().val;
     }
 }
