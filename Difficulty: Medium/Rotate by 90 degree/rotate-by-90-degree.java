6487e136dfda782ac89abb3a943e6c53986eed6f
P1 <87040096+pawanpk87@users.noreply.github.com>
2024-11-06 01:28:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate by 90 degree/rotate-by-90-degree.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class DriverClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[][] arr = new int[n][n];
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++) arr[i][j] = sc.nextInt();
+
+            GFG g = new GFG();
+            g.rotate(arr);
+            printMatrix(arr);
+
+            System.out.println("~");
+        }
+    }
+
+    static void printMatrix(int arr[][]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) System.out.print(arr[i][j] + " ");
+            System.out.println("");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class GFG {
+    static void rotate(int mat[][]) {
+        int n = mat.length;
+        int m = mat[0].length;
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < m; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m/2; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[i][m - j - 1];
+                mat[i][m - j - 1] = temp;
+            }
+        }
+    }
+}
