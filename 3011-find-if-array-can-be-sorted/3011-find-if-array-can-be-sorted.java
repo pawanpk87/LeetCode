7452c83e3f5ae4af04ef713726b4a5ae2b9f1535
P1 <87040096+pawanpk87@users.noreply.github.com>
2024-11-06 21:38:38 +0530

    Time: 1 ms (100.00%), Space: 44.2 MB (76.10%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.java
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.java
@@ -2,50 +2,33 @@ class Solution {
     public boolean canSortArray(int[] nums) {
         int n = nums.length;
         
-        int[] setBitCount = new int[n];
-        for(int i = 0; i < n; i++) {
-            setBitCount[i] = getSetBitCount(nums[i]);
-        }
+        int minOfSegment = nums[0];
+        int maxOfSegment = nums[0];
+    
+        int maxOfPrevSegment = Integer.MIN_VALUE;
         
-        int prevIndex = 0;
-        int currBitCounts = setBitCount[0];
+        int bitCount = Integer.bitCount(nums[0]);
         
         for(int i = 1; i < n; i++) {
-            if(setBitCount[i] != currBitCounts) {
-                sortArr(nums, prevIndex, i);
-                currBitCounts = setBitCount[i];
-                prevIndex = i;
+            if(bitCount == Integer.bitCount(nums[i])) {
+                minOfSegment = Math.min(minOfSegment, nums[i]);
+                maxOfSegment = Math.max(maxOfSegment, nums[i]);
+            } else {
+                if(minOfSegment < maxOfPrevSegment) {
+                    return false;
+                }
+                
+                maxOfPrevSegment = maxOfSegment;
+                
+                minOfSegment = nums[i];
+                maxOfSegment = nums[i];
+                
+                bitCount = Integer.bitCount(nums[i]);
             }
         }
         
-        sortArr(nums, prevIndex, n);
-        
-        return isSorted(nums);
-    }
-    
-    private void sortArr(int[] nums, int startIndex, int endIndex) {
-        Arrays.sort(nums, startIndex, endIndex);
-    }
-    
-    private int getSetBitCount(int num) {
-        int count = 0;
-        
-        for(int i = 0; i < 32; i++) {
-            if((num & (1 << i)) != 0) {
-                count++;
-            }
-        }
-        
-        return count;
-    }
-    
-    private boolean isSorted(int[] nums) {
-        int n = nums.length;
-        
-        for(int i = 1; i < n; i++) {
-            if(nums[i - 1] > nums[i]) {
-                return false;
-            }
+        if(minOfSegment < maxOfPrevSegment) {
+            return false;
         }
         
         return true;
