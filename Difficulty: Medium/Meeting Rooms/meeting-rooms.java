ccebf7fcfaec6ddd470c885d36485377d13849d7
P1 <87040096+pawanpk87@users.noreply.github.com>
2024-11-12 23:05:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Meeting Rooms/meeting-rooms.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] arr = new int[n][2];
+            for (int i = 0; i < n; i++) {
+                String temp[] = br.readLine().trim().split(" ");
+                arr[i][0] = Integer.parseInt(temp[0]);
+                String x = temp[1];
+                arr[i][1] = Integer.parseInt(x);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.canAttend(arr);
+            if (ans)
+                System.out.println("true");
+            else
+                System.out.println("false");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    static boolean canAttend(int[][] arr) {
+        
+        Arrays.sort(arr, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b) {
+                if(a[0] == b[0]) {
+                    return a[1] - b[1];
+                }
+                return a[0] - b[0];
+            } 
+        });
+        
+        int n = arr.length;
+    
+        for(int i = 1; i < n; i++) {
+            if(arr[i][0] < arr[i - 1][1]) {
+                return false;
+            }        
+        }
+        
+        return true;
+    }
+}
