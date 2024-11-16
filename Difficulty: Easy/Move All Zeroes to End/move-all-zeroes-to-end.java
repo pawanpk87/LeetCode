e2f19fb9e629ae49410166b2585292fec8dc3797
P1 <87040096+pawanpk87@users.noreply.github.com>
2024-11-16 20:55:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            String[] inputLine = br.readLine().split(" ");
+            int[] arr = new int[inputLine.length];
+            for (int i = 0; i < inputLine.length; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().pushZerosToEnd(arr);
+            int n = arr.length;
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        int n = arr.length;
+        
+        int i = 0;
+        
+        while(i < n && arr[i] != 0) {
+            i++;
+        }
+        
+        int j = i + 1;
+        
+        while(j < n) {
+            while(j < n && arr[j] == 0) {
+                j++;
+            }
+            
+            if(j < n) {
+                swap(arr, i, j);
+            }
+            
+            i++; j++;
+        }
+    }
+    
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
