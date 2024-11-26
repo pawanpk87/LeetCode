20198e597dd93935019bae559ed6a110138813d6
P1 <87040096+pawanpk87@users.noreply.github.com>
2024-11-26 09:41:30 +0530

    Time: 1 ms (100.00%), Space: 46.1 MB (15.44%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.java
+++ 2924-find-champion-ii/2924-find-champion-ii.java
@@ -4,27 +4,19 @@ class Solution {
         
         int u,v;
         for(int[] edge : edges) {
-            u = edge[0]; v = edge[1];
+            u = edge[0];
+            v = edge[1];
             indegree[v]++;
         }
-
+        
         int champion = -1;
-        int minIndegree = Integer.MAX_VALUE;
         
         for(int i = 0; i < n; i++) {
-            if(minIndegree > indegree[i]) {
-                minIndegree = indegree[i];
+            if(indegree[i] == 0) {
+                if(champion != -1) {
+                    return -1;
+                }
                 champion = i;
-            }
-        }
-        
-        for(int i = 0; i < n; i++) {
-            if(champion == i) {
-                continue;
-            }
-            
-            if(indegree[i] <= minIndegree) {
-                return -1;
             }
         }
         
