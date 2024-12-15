fc6f809a1ba9bc62cd4247e142920cfe20cb4634
P1 <87040096+pawanpk87@users.noreply.github.com>
2024-12-15 11:46:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Peak element/peak-element.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String arr[] = br.readLine().split(" ");
+            int a[] = new int[arr.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                a[i] = Integer.parseInt(arr[i]);
+            }
+            Solution obj = new Solution();
+            int f = 0;
+            int idx = obj.peakElement(a);
+            int n = a.length;
+            if (idx < 0 && idx >= n)
+                System.out.println("false");
+            else {
+                if (n == 1 && idx == 0)
+                    f = 1;
+                else if (idx == 0 && a[0] > a[1])
+                    f = 1;
+                else if (idx == n - 1 && a[n - 1] > a[n - 2])
+                    f = 1;
+                else if (idx > 0 && idx < n && a[idx] > a[idx + 1] &&
+                         a[idx] > a[idx - 1])
+                    f = 1;
+                else
+                    f = 0;
+                if (f == 1) {
+                    System.out.println("true");
+                } else {
+                    System.out.println("false");
+                }
+            }
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    public int peakElement(int[] arr) {
+        int n = arr.length;
+        
+        if(n == 1) {
+            return 0;
+        }
+        
+        int prev, next;
+        for(int i = 0; i < n; i++) {
+            if(i == 0) {
+                prev = Integer.MIN_VALUE;
+                next = arr[i + 1];
+            } else if(i == n - 1) {
+                prev = arr[i - 1];
+                next = Integer.MIN_VALUE;
+            } else {
+                prev = arr[i - 1];
+                next = arr[i + 1];
+            }
+            
+            if(isPeakElement(prev, arr[i], next)) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+    
+    private boolean isPeakElement(int a, int b, int c) {
+        return a < b && b > c;
+    }
+}
